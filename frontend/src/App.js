6c6d32a3556b76651b6f3ba2d23a6c4ef0ade3b7
Your Namebolanissakomlan5 <you@example.com>
2024-07-30 22:29:39 +0000

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona2</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
